487bd15fcba4114310f73fdfa1d793a1167b08bf
Raphael Nembhard <nembhardraphael199@gmail.com>
2025-03-29 15:43:00 -0400

    added override fee

--- src/SwoopPon.sol
+++ src/SwoopPon.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import {BaseDynamicAfterFee} from "../lib/uniswap-hooks/src/fee/BaseDynamicAfterFee.sol";
+import {BaseOverrideFee} from "../lib/uniswap-hooks/src/fee/BaseOverrideFee.sol";
 import {PoolKey} from "../lib/uniswap-hooks/lib/v4-core/src/types/PoolKey.sol";
 import {IPoolManager} from "../lib/uniswap-hooks/lib/v4-core/src/interfaces/IPoolManager.sol";
 import {CurrencyLibrary, Currency} from "../lib/uniswap-hooks/lib/v4-core/src/types/Currency.sol";
@@ -9,10 +9,11 @@ import {BalanceDeltaLibrary, BalanceDelta} from "../lib/uniswap-hooks/lib/v4-cor
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "../lib/uniswap-hooks/lib/v4-core/src/types/BeforeSwapDelta.sol";
 
 
-contract SwoopPon is BaseDynamicAfterFee {
+contract SwoopPon is BaseOverrideFee {
     using CurrencyLibrary for Currency;
     using BalanceDeltaLibrary for BalanceDelta;
-    constructor(IPoolManager _poolManager) BaseDynamicAfterFee(_poolManager) {
+    
+    constructor(IPoolManager _poolManager) BaseOverrideFee(_poolManager) {
         
     }
 
@@ -29,8 +30,8 @@ contract SwoopPon is BaseDynamicAfterFee {
         IPoolManager.SwapParams calldata params,
         bytes calldata hookData
     ) internal virtual override returns (bytes4, BeforeSwapDelta, uint24) {
-        // Your logic here
-        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        uint24 fee = _getFee(sender, key, params, hookData);
+        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, fee);
     }
 
     function _afterSwap(
@@ -41,30 +42,18 @@ contract SwoopPon is BaseDynamicAfterFee {
         bytes calldata hookData
     ) internal virtual override returns (bytes4, int128) {
         // Your logic here
+        // Mint tokens to 
         return (this.afterSwap.selector, 0);
     }
 
-    function _getTargetOutput(
+    function _getFee(
         address sender,
         PoolKey calldata key,
         IPoolManager.SwapParams calldata params,
         bytes calldata hookData
-    ) internal virtual override returns (uint256 targetOutput, bool applyTargetOutput) {
-        // For now, we're not modifying the output, so we return false
-        return (0, false);
+    ) internal virtual override returns (uint24) {
+        // Return the current fee
+        return _fee;
     }
-
-    function _afterSwapHandler(
-        PoolKey calldata key,
-        IPoolManager.SwapParams calldata params,
-        BalanceDelta delta,
-        uint256 targetOutput,
-        uint256 feeAmount
-    ) internal virtual override {
-        // Here you can implement your token minting logic
-        // For example:
-        // rewardToken.mint(params.recipient, 1 ether);
-    }
-      
     
 }
