d964507b65244e402a75b42bbb26fd64d68315d0
Raphael Nembhard <nembhardraphael199@gmail.com>
2025-03-29 22:49:36 -0400

    test setup

--- test/SwoopPon.t.sol
+++ test/SwoopPon.t.sol
@@ -15,18 +15,33 @@ import {Deployers} from "../lib/uniswap-hooks/lib/v4-core/test/utils/Deployers.s
 import {BaseOverrideFeeMock} from "../lib/uniswap-hooks/test/mocks/BaseOverrideFeeMock.sol";
 import {IHooks} from "../lib/uniswap-hooks/lib/v4-core/src/interfaces/IHooks.sol";
 import {LPFeeLibrary} from "../lib/uniswap-hooks/lib/v4-core/src/libraries/LPFeeLibrary.sol";
-import {MockOracleBTC} from "test/Mockoracle/MockOracleBTC.sol";
-import {MockOracleETH} from "test/Mockoracle/MockOracleETH.sol";
+import {MockOracleBTC} from "test/mocks/MockOracleBTC.sol";
+import {MockOracleETH} from "test/mocks/MockOracleETH.sol";
+import {PoolSwapTest} from "../lib/uniswap-hooks/lib/v4-core/src/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
 
 contract SwoopPonTest is Test, Deployers {
     BaseOverrideFeeMock swoopPon;
 
-    MockOracleETH oracle;
+    MockERC20 token;
 
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency;
+
+    MockOracleETH oracle;
     MockOracleBTC oracle2;
 
     function setUp() public {
         deployFreshManagerAndRouters();
+
+        // Deploy our TOKEN contract
+        token = new MockERC20("SwoopPon", "Sp", 18);
+        tokenCurrency = Currency.wrap(address(token));
+
+        // Mint a bunch of TOKEN to ourselves and to address(1)
+        token.mint(address(this), 1000 ether);
+        token.mint(address(1), 1000 ether);
 
         swoopPon = BaseOverrideFeeMock(address(uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_INITIALIZE_FLAG)));
         deployCodeTo("test/mocks/BaseOverrideFeeMock.sol:BaseOverrideFeeMock", abi.encode(manager), address(swoopPon));
@@ -39,6 +54,15 @@ contract SwoopPonTest is Test, Deployers {
         vm.label(Currency.unwrap(currency0), "currency0");
         vm.label(Currency.unwrap(currency1), "currency1");
 
+        // Initialize a pool
+        (key, ) = initPool(
+            ethCurrency, // Currency 0 = ETH
+            tokenCurrency, // Currency 1 = TOKEN
+            swoopPon, // Hook Contract
+            LPFeeLibrary.DYNAMIC_FEE_FLAG, // Swap Fees
+            SQRT_PRICE_1_1 // Initial Sqrt(P) value = 1
+        );
+
         oracle = new MockOracleETH();
         oracle2 = new MockOracleBTC();
     }
@@ -50,6 +74,15 @@ contract SwoopPonTest is Test, Deployers {
     }
 
     // function test_swap_with_no_deposit() public {
+    //     PoolSwapTest.TestSettings memory testSettings = PoolSwapTest
+    //         .TestSettings({takeClaims: false, settleUsingBurn: false});
+
+    //     IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
+    //         zeroForOne: true,
+    //         amountSpecified: -0.00001 ether,
+    //         sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+    //     });
+
     //     vm.startPrank(user);
     //     manager.swap(currency0, currency1, 100, 0);
     //     vm.stopPrank();
